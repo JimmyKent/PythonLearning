6a3024b07d7bbed7897ab341fad4a28f944f47e0
金闪闪 <jinguochong@gmail.com>
2017-12-17 22:19:47 +0800

    Update Problem83.py
    Problem83

--- com/jimmy/euler_project/Problem83.py
+++ com/jimmy/euler_project/Problem83.py
@@ -20,10 +20,15 @@ def read_data(filename, split):
     return np.mat(matrix_x)
 
 
-class Node:
+class Node(object):
     x = 0
     y = 0
     distance = 0
+
+    def __init__(self, x, y, distance):
+        self.x = x
+        self.y = y
+        self.distance = distance
 
     def __eq__(self, other):
         return self.x * 100 + self.y == other.x * 100 + other.y
@@ -35,15 +40,101 @@ class Node:
         return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.distance) + ")"
 
 
+def refresh(node):
+    temp = DEFAULT_DISTANCE
+    temp = min(getDistance(node.x, node.y - 1), temp)
+    temp = min(getDistance(node.x, node.y + 1), temp)
+    temp = min(getDistance(node.x - 1, node.y), temp)
+    temp = min(getDistance(node.x + 1, node.y), temp)
+    return temp
+
+
+def getDistance(x, y):
+    if (y < 0 or y >= 80 or x < 0 or x > 80):
+        return DEFAULT_DISTANCE
+    else:
+        return disMatrix[x, y]
+
+
+# ----------------------------------
+
+
 original = read_data("p81-82-83_matrix.txt", ',')
 
 DEFAULT_DISTANCE = 100000000
-print(sys.maxsize / 2)
 
-disMatrix = np.copy(original)
+disMatrix = np.full(original.shape, DEFAULT_DISTANCE, dtype=int)
+
+# print(disMatrix)
+# print(original)
+
+# init
+disMatrix[0, 0] = original[0, 0]
+
+U = []
+# XXX
+for h in range(80):
+    for w in range(80):
+        U.append(Node(h, w, DEFAULT_DISTANCE))
+
+heapify(U)
+U.remove(Node(0, 0, DEFAULT_DISTANCE))
+U.remove(Node(0, 1, DEFAULT_DISTANCE))
+U.remove(Node(1, 0, DEFAULT_DISTANCE))
+
+heappush(U, Node(0, 1, original[0, 1] + original[0, 0]))
+disMatrix[0, 1] = original[0, 1] + original[0, 0]
+heappush(U, Node(1, 0, original[1, 0] + original[0, 0]))
+disMatrix[1, 0] = original[1, 0] + original[0, 0]
 
 print(disMatrix)
 
+while (len(U) != 0):
+    node = heappop(U)
+    w = node.x
+    h = node.y
+    disMatrix[h, w] = node.distance
+
+    print(node)
+
+    if (w - 1 > 0 and w - 1 < 80):
+        left = Node(h, w - 1, disMatrix[h, w - 1])
+        if (left in U):
+            temp = refresh(left)
+            disMatrix[h, w - 1] = min(original[h, w - 1] + temp, disMatrix[h, w - 1])
+            left.distance = disMatrix[h, w - 1]
+            U.remove(left)
+            heappush(U, left)
+
+    if (w + 1 > 0 and w + 1 < 80):
+        right = Node(h, w + 1, disMatrix[h, w + 1])
+        if (right in U):
+            temp = refresh(right)
+            disMatrix[h, w + 1] = min(original[h, w + 1] + temp, disMatrix[h, w + 1])
+            right.distance = disMatrix[h, w + 1]
+            U.remove(right)
+            heappush(U, right)
+
+    if (h - 1 > 0 and h - 1 < 80):
+        top = Node(h -1, w, disMatrix[h - 1, w])
+        if (top in U):
+            temp = refresh(top)
+            disMatrix[h - 1, w] = min(original[h - 1, w] + temp, disMatrix[h - 1, w])
+            top.distance = disMatrix[h - 1, w]
+            U.remove(top)
+            heappush(U, top)
+
+    if (h + 1 > 0 and h +  1 < 80):
+        bottom = Node(h + 1, w, disMatrix[h + 1, w])
+        if (bottom in U):
+            temp = refresh(bottom)
+            disMatrix[h + 1, w] = min(original[h + 1, w] + temp, disMatrix[h + 1, w])
+            bottom.distance = disMatrix[h + 1, w]
+            U.remove(bottom)
+            heappush(U, bottom)
+
+
+print(disMatrix[79, 79])
 # 获得矩阵的行数
 # for i in range(1, Y.shape[0]):
 #     Y[i, 0] += Y[i - 1, 0]
