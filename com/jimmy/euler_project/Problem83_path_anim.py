6c7ac9144ccf3e37547a70446db3694e298ef5dc
JimmyKent <jinguochong@gmail.com>
2017-12-22 16:37:46 +0800

    Problem83_test

--- com/jimmy/euler_project/Problem83_path_anim.py
+++ com/jimmy/euler_project/Problem83_path_anim.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pylab import *
 from matplotlib import pyplot as plt
+from matplotlib import animation
+plt.rcParams['animation.ffmpeg_path'] = '/usr/local/Cellar/ffmpeg/3.4.1/bin/ffmpeg'
 
 disMatrix = np.loadtxt("/Users/jinguochong/PycharmProjects/PythonLearning/com/jimmy/euler_project/Problem83_distance_matrix.txt", delimiter=' ')
 
@@ -52,11 +54,42 @@ plot(path_x, path_y, 'r*')
 # ∈
 # correct 将数组中的前两个点进行连线 x0,y0 -- x1,y1   x[0, 2] == x ∈ [0, 2)
 # plot(path_x[:], path_y[:])
-# show()
 # 打点 Dijkstra 遍历过程
 for i in range(len(path_x)):
     x = len(path_x) - i
     plot(path_x[x - 2: x], path_y[x - 2: x], 'b')
     plt.pause(0.005)
+
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+line = ax.plot([], [], lw=2)
+
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=20, blit=True)
+
+# save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# installed.  The extra_args ensure that the x264 codec is used, so that
+# the video can be embedded in html5.  You may need to adjust this for
+# your system: for more information, see
+# http://matplotlib.sourceforge.net/api/animation_api.html
+
+anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 plt.show()
