c1e129132894be624dcc6f57ff96865e117e3e9e
JimmyKent <jinguochong@gmail.com>
2018-01-16 18:05:21 +0800

    create db

--- com/jimmy/spider/QsbkHot.py
+++ com/jimmy/spider/QsbkHot.py
@@ -16,25 +16,69 @@ __author__ = 'Jimmy'
 
 import urllib.request as urllib2
 import re
+import sqlite3
 
-page = 1
-url = 'https://www.qiushibaike.com/hot/page/' + str(page)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'User-Agent': user_agent}
-try:
-    request = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(request)
-    content = response.read().decode('utf-8')
-    # print(content)
 
-    pattern = re.compile('<span>(.*?)</span>', re.S)
-    items = re.findall(pattern, content)
-    for item in items:
-        print(item.strip())
-        print("-----")
+def get_qsbk_list(page):
+    if page < 1:
+        page = 1
+    url = 'https://www.qiushibaike.com/hot/page/' + str(page)
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent}
+    _list = []
+    try:
+        request = urllib2.Request(url, headers=headers)
+        response = urllib2.urlopen(request)
+        content = response.read().decode('utf-8')
+        # print(content)
 
-except urllib2.URLError as e:
-    if hasattr(e, "code"):
-        print(e.code)
-    if hasattr(e, "reason"):
-        print(e.reason)
+        pattern = re.compile('<span>(.*?)</span>', re.S)
+        items = re.findall(pattern, content)
+        for item in items:
+            # print(item.strip())
+            _list.append(item.strip())
+
+    except urllib2.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+
+    return _list
+
+
+def save_to_db():
+    # 连接到SQLite数据库
+    # 数据库文件是test.db
+    # 如果文件不存在，会自动在当前目录创建:
+    conn = sqlite3.connect('test.db')
+    # 创建一个Cursor:
+    cursor = conn.cursor()
+    # 执行一条SQL语句，创建user表:
+    cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+
+    # 继续执行一条SQL语句，插入一条记录:
+    cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+    # 关闭Cursor:
+    cursor.close()
+    # 提交事务:
+    conn.commit()
+    # 关闭Connection:
+    conn.close()
+
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    # 执行查询语句:
+    cursor.execute('select * from user where id=?', ('1',))
+    # 获得查询结果集:
+    values = cursor.fetchall()
+    print(values)
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    print("this is program entrance")
+    _list = get_qsbk_list(1)
+    # print(_list.__sizeof__())
+    save_to_db()
